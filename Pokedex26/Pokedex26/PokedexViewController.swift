0ec23ffd2d533d7109fe6b530f2046f3b5702bd9
Trevor Adcock <trevor.adcock@mac.com>
2019-05-21 09:41:41 -0600

    Adds Swift

---
+++ Pokedex26/Pokedex26/PokedexViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Pokedex26
+//
+//  Created by DevMountain on 5/21/19.
+//  Copyright © 2019 trevorAdcock. All rights reserved.
+//
+
+import UIKit
+
+class PokedexViewController: UIViewController {
+    
+    @IBOutlet weak var pokeImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var idLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var pokeSearchBar : UISearchBar!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
