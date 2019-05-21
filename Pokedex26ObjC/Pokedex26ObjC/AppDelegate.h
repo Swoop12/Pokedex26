663465ca8cf250a505d7ac8a04dd058a26b3fb70
Trevor Adcock <trevor.adcock@mac.com>
2019-05-21 09:37:58 -0600

    Initial Commit

---
+++ Pokedex26ObjC/Pokedex26ObjC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Pokedex26ObjC
+//
+//  Created by DevMountain on 5/21/19.
+//  Copyright © 2019 trevorAdcock. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
